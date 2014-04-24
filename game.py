292442e5375c3c3090a9a35d52cd2f4e28c74184
Andrei Petre <p31andrei@gmail.com>
2014-04-24 09:59:16 +0300

    Draw preys and predators on the map, no movement.

--- game.py
+++ game.py
@@ -16,23 +16,45 @@ def get_config(config_file='config.rc'):
     return config
 
 def objects_collide(pos1, rad1, pos2, rad2):
-    pass
+    '''Detects if two objects touch another.'''
+    # TODO
+    return False
 
-def random_generate_game(config):
-    # generate pray's position
+def set_initial_positions(config):
+    '''Generate random positions for pray and attackers so that
+    no one collides with another one, initially.
+    '''
     x,y = config.getint('game','x'), config.getint('game','y')
 
+    # generate pray's position
     pray_coord = (random.random()*x, random.random()*y)
-    pred_coord = []
-    #while len(pred_coord) < config.getint('game','pred_nr'):
-    #    coord = (random.random()*x, random.random()*y)
-    #    pred_coord += coord
-
+    pray_collide_radius = config.getint('game','pray_collision')
     config.set('game','pray_coord', pray_coord)
+
+    # generate all predators w/o collision w/ existing ones.
+    pred_coords = []
+    pred_collide_radius = config.getint('game','pred_collision')
+    while len(pred_coords) < config.getint('game','pred_nr'):
+        coord = (random.random()*x, random.random()*y)
+        # new generated coord should not conflict w/ prey.
+        if objects_collide(pray_coord, pray_collide_radius,
+                            coord, pred_collide_radius):
+            continue
+        # new generated coord should not conflict w/ generated pred.
+        is_collision = False
+        for coord2 in pred_coords:
+            if objects_collide(coord2, pred_collide_radius,
+                               coord, pred_collide_radius):
+                is_collision = True
+                break
+        if not is_collision:
+            pred_coords.append(coord)
+
+    config.set('game','pred_coords', pred_coords)
 
 if __name__ == '__main__':
     config = get_config()
-    random_generate_game(config)
+    set_initial_positions(config)
     Draw(config)
     #while True:
     #    pass
--- ui/game.py
+++ ui/game.py
@@ -3,24 +3,23 @@ from pygame.locals import *
 
 BLACK=(0,0,0)
 WHITE=(255,255,255)
-RED=(255, 0, 0)
+RED=(255,0,0)
 PINK=(255,0,128)
 LIGHTBLUE=(0,255,255)
-
-SCALE=5
+YELLOW=(255,255,0)
+BLUE=(0,0,255)
 
 PERCEPT_COLOR=LIGHTBLUE
 COLLIDE_COLOR=PINK
-PRAY_COLOR=WHITE
-PRED_COLOR=RED
+PRAY_COLOR=YELLOW
+PRED_COLOR=BLUE
 
 class Draw:
     def __init__(self, config):
         self.config = config
-        resolution = (config.getint('game','x')*SCALE,
-                      config.getint('game','y')*SCALE)
+        resolution = (config.getint('game','x'),
+                      config.getint('game','y'))
         self.screen = pygame.display.set_mode(resolution, DOUBLEBUF)
-        print config.get('game','pray_coord')
         self.run()
 
     def run(self):
@@ -42,24 +41,36 @@ class Draw:
                 if event.key == K_ESCAPE:
                     running = False
                     break
-
             self.screen.fill(BLACK)
             self.draw_once()
             pygame.display.flip()
+
         pygame.quit()
 
     def draw_once(self):
-            pray_percept_radius = self.config.getint('game','pray_perception')
-            # Draw perception, collision, shape
-            pygame.draw.circle(self.screen, PERCEPT_COLOR,
-                               self.get_pray_coord(),
-                               SCALE * pray_percept_radius, 1)
-            pray_collide_radius = self.config.getint('game','pray_collision')
-            pygame.draw.circle(self.screen, COLLIDE_COLOR,
-                               self.get_pray_coord(),
-                               SCALE * pray_collide_radius, 1)
-            pygame.draw.circle(self.screen, PRAY_COLOR, self.get_pray_coord(), SCALE)
+        pray_percept_radius = self.config.getint('game','pray_perception')
+        pray_collide_radius = self.config.getint('game','pray_collision')
+        pray_coord = self.config.get('game', 'pray_coord')
+        self.draw_object(self.get_coord(pray_coord),
+                         [PERCEPT_COLOR, COLLIDE_COLOR, PRAY_COLOR],
+                         [pray_percept_radius, pray_collide_radius,
+                          pray_collide_radius / 2])
+        pred_percept_radius = self.config.getint('game','pred_perception')
+        pred_collide_radius = self.config.getint('game','pred_collision')
+        for i in range(self.config.getint('game','pred_nr')):
+            pred_coord = self.config.get('game','pred_coords')[i]
+            self.draw_object(self.get_coord(pred_coord),
+                             [PERCEPT_COLOR, COLLIDE_COLOR, PRED_COLOR],
+                             [pred_percept_radius, pred_collide_radius,
+                              pred_collide_radius / 2])
 
-    def get_pray_coord(self):
-        (a,b) = self.config.get('game', 'pray_coord')
+    def draw_object(self, coords, colors, dimensions):
+        '''Draw two outer shells, then the object.'''
+        pygame.draw.circle(self.screen, colors[0], coords, dimensions[0], 1)
+        pygame.draw.circle(self.screen, colors[1], coords, dimensions[1], 1)
+        pygame.draw.circle(self.screen, colors[2], coords, dimensions[2])
+
+    def get_coord(self, coord):
+        '''Pygame knows only how to draw w/ integer coords.'''
+        (a,b) = coord
         return (int(a),int(b))
