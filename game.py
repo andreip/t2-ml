26203294e6a0eb789c5828fc8b7e870130fefe01
Andrei Petre <p31andrei@gmail.com>
2014-04-24 10:44:57 +0300

    Draw traps too.

--- game.py
+++ game.py
@@ -20,37 +20,55 @@ def objects_collide(pos1, rad1, pos2, rad2):
     # TODO
     return False
 
+def generate_without_collision(collision_list, collision_radius):
+    '''Generate a new coordinate so that it does not collide with any
+    of the coords from collision_list.
+    '''
+    x,y = config.getint('game','x'), config.getint('game','y')
+    while True:
+        coord = (random.random()*x, random.random()*y)
+        # new generated coord should not conflict w/ collision list.
+        for (coord2, collision_radius2) in collision_list:
+            if objects_collide(coord2, collision_radius2,
+                               coord, collision_radius):
+                break
+        # did not break, thus no collision w/ any of coords from list.
+        else:
+            return coord
+
 def set_initial_positions(config):
     '''Generate random positions for pray and attackers so that
     no one collides with another one, initially.
     '''
-    x,y = config.getint('game','x'), config.getint('game','y')
-
     # generate pray's position
-    pray_coord = (random.random()*x, random.random()*y)
     pray_collide_radius = config.getint('game','pray_collision')
+    pray_coord = generate_without_collision([], pray_collide_radius)
     config.set('game','pray_coord', pray_coord)
 
     # generate all predators w/o collision w/ existing ones.
     pred_coords = []
     pred_collide_radius = config.getint('game','pred_collision')
     while len(pred_coords) < config.getint('game','pred_nr'):
-        coord = (random.random()*x, random.random()*y)
-        # new generated coord should not conflict w/ prey.
-        if objects_collide(pray_coord, pray_collide_radius,
-                            coord, pred_collide_radius):
-            continue
-        # new generated coord should not conflict w/ generated pred.
-        is_collision = False
-        for coord2 in pred_coords:
-            if objects_collide(coord2, pred_collide_radius,
-                               coord, pred_collide_radius):
-                is_collision = True
-                break
-        if not is_collision:
-            pred_coords.append(coord)
-
+        # Create a list of the form (coord, collision_radius) to pass to
+        # coord generator.
+        collision_list =\
+           [(pray_coord, pray_collide_radius)] +\
+           map(lambda c: (c, pred_collide_radius), pred_coords)
+        coord = generate_without_collision(collision_list, pred_collide_radius)
+        pred_coords.append(coord)
     config.set('game','pred_coords', pred_coords)
+
+    # generate all traps w/o collision w/ pray/predators.
+    trap_coords = []
+    trap_collide_radius = config.getint('game','trap_collision')
+    while len(trap_coords) < config.getint('game','trap_nr'):
+        collision_list =\
+           [(pray_coord, pray_collide_radius)] +\
+           map(lambda c: (c, pred_collide_radius), pred_coords) +\
+           map(lambda c: (c, trap_collide_radius), trap_coords)
+        coord = generate_without_collision(collision_list, trap_collide_radius)
+        trap_coords.append(coord)
+    config.set('game','trap_coords', trap_coords)
 
 if __name__ == '__main__':
     config = get_config()
--- ui/game.py
+++ ui/game.py
@@ -8,11 +8,13 @@ PINK=(255,0,128)
 LIGHTBLUE=(0,255,255)
 YELLOW=(255,255,0)
 BLUE=(0,0,255)
+GRAY=(128,128,128)
 
-PERCEPT_COLOR=LIGHTBLUE
-COLLIDE_COLOR=PINK
+PERCEPT_COLOR=GRAY
+COLLIDE_COLOR=RED
 PRAY_COLOR=YELLOW
 PRED_COLOR=BLUE
+TRAP_COLOR=GRAY
 
 class Draw:
     def __init__(self, config):
@@ -48,27 +50,44 @@ class Draw:
         pygame.quit()
 
     def draw_once(self):
+        '''Draw objects like pray, predators, traps on the map.'''
+        # Draw pray
         pray_percept_radius = self.config.getint('game','pray_perception')
         pray_collide_radius = self.config.getint('game','pray_collision')
         pray_coord = self.config.get('game', 'pray_coord')
-        self.draw_object(self.get_coord(pray_coord),
-                         [PERCEPT_COLOR, COLLIDE_COLOR, PRAY_COLOR],
-                         [pray_percept_radius, pray_collide_radius,
-                          pray_collide_radius / 2])
+        self.draw_creature(self.get_coord(pray_coord),
+                          [PERCEPT_COLOR, COLLIDE_COLOR, PRAY_COLOR],
+                          [pray_percept_radius, pray_collide_radius,
+                           pray_collide_radius / 2])
+        # Draw predators
         pred_percept_radius = self.config.getint('game','pred_perception')
         pred_collide_radius = self.config.getint('game','pred_collision')
         for i in range(self.config.getint('game','pred_nr')):
             pred_coord = self.config.get('game','pred_coords')[i]
-            self.draw_object(self.get_coord(pred_coord),
-                             [PERCEPT_COLOR, COLLIDE_COLOR, PRED_COLOR],
-                             [pred_percept_radius, pred_collide_radius,
-                              pred_collide_radius / 2])
+            self.draw_creature(self.get_coord(pred_coord),
+                              [PERCEPT_COLOR, COLLIDE_COLOR, PRED_COLOR],
+                              [pred_percept_radius, pred_collide_radius,
+                               pred_collide_radius / 2])
+        # Draw traps
+        trap_collide_radius = self.config.getint('game','trap_collision')
+        for i in range(self.config.getint('game','trap_nr')):
+            trap_coord = self.config.get('game','trap_coords')[i]
+            self.draw_trap(self.get_coord(trap_coord),
+                          [COLLIDE_COLOR, TRAP_COLOR],
+                          [trap_collide_radius, trap_collide_radius / 2])
 
-    def draw_object(self, coords, colors, dimensions):
-        '''Draw two outer shells, then the object.'''
+    def draw_creature(self, coords, colors, dimensions):
+        '''Draw a creature: two outer shells: perception, collision,
+        (draw perception first as perception should be > collision)
+        then the creature.'''
         pygame.draw.circle(self.screen, colors[0], coords, dimensions[0], 1)
         pygame.draw.circle(self.screen, colors[1], coords, dimensions[1], 1)
         pygame.draw.circle(self.screen, colors[2], coords, dimensions[2])
+
+    def draw_trap(self, coords, colors, dimensions):
+        '''Draw a trap: one outer shell: collision, then the trap.'''
+        pygame.draw.circle(self.screen, colors[0], coords, dimensions[0], 1)
+        pygame.draw.circle(self.screen, colors[1], coords, dimensions[1])
 
     def get_coord(self, coord):
         '''Pygame knows only how to draw w/ integer coords.'''
